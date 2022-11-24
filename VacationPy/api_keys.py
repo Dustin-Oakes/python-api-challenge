35e47eaa3f893bebda434699ff0e51c67f845d83
Dustin-Oakes <Oakes.Dustin@gmail.com>
2022-11-23 19:33:46 -0800

    Additional changes to VacationPy and adding meatmap images

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE"
+
+# Geoapify API Key
+geoapify_key = "3e22955b30d841e59cdca2e42a2e3107"
+
+# gmaps key
+gmaps_key = "AIzaSyAHREwHvnurJbBwpr0W1c7Wgb5Y3NzlCtw"
